5f0360fc339bf6f621ab7f39afd5c7d0e83a832a
igor.wolkoff@gmail.com <igor.wolkov@kredito.de>
2017-10-21 01:53:08 +0300

    Workshop template

--- src/main/scala/karazinscalausersgroup/workshop/FlowMonoid.scala
+++ src/main/scala/karazinscalausersgroup/workshop/FlowMonoid.scala
@@ -20,31 +20,5 @@ object FlowMonoid extends App {
   val toUpperCase: Flow = s => s.toUpperCase
   val toLowerCase: Flow = s => s.toUpperCase
 
-  implicit val flowMonoid: Monoid[Flow] =
-    new Monoid[Flow] {
-      def empty = unit
-
-      def combine(x: Flow, y: Flow) =
-        x compose y
-    }
-
-  val flow = trim |+| toUpperCase
-
-  println(flow("  hello world  "))
-
-
-  implicit val flowEq: Eq[Flow] =
-    new Eq[Flow] {
-      def eqv(x: Flow, y: Flow) = {
-
-        (1 to 100) forall { _ =>
-          val s = Random.nextString(Random.nextInt(100))
-
-          x(s) == y(s)
-        }
-      }
-    }
-
-  GroupLaws[Flow].monoid(Monoid[Flow]).all.check()
 
 }
