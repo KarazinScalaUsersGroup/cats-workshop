0286982630d7e117075d26aafe5ae70b3baa6716
igor.wolkoff@gmail.com <igor.wolkov@kredito.de>
2017-10-26 16:47:31 +0300

    05 Max semigroup

--- src/main/scala/karazinscalausersgroup/workshop/CatsWorkshop.scala
+++ src/main/scala/karazinscalausersgroup/workshop/CatsWorkshop.scala
@@ -5,4 +5,19 @@ package karazinscalausersgroup.workshop
   */
 object CatsWorkshop extends App {
 
+  import cats.kernel.{Eq, Semigroup}
+  import cats.kernel.laws.GroupLaws
+  import cats.syntax.semigroup._
+
+  implicit val maxSemigroup: Semigroup[Int] =
+    (x: Int, y: Int) => Math.max(x, y)
+
+  println(42 |+| 54)
+  println(54 |+| 42)
+
+  implicit val intEq: Eq[Int] =
+    (x: Int, y: Int) => x == y
+
+  GroupLaws[Int].semigroup(Semigroup[Int]).all.check()
+
 }
