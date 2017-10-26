5f375c8689b70127bb84812a2a597c6a9cbe34d5
igor.wolkoff@gmail.com <igor.wolkov@kredito.de>
2017-10-26 16:45:57 +0300

    04 Stats semigroup

--- src/main/scala/karazinscalausersgroup/workshop/CatsWorkshop.scala
+++ src/main/scala/karazinscalausersgroup/workshop/CatsWorkshop.scala
@@ -1,8 +1,80 @@
 package karazinscalausersgroup.workshop
 
+import cats.kernel.laws.GroupLaws
+import cats.kernel.{Eq, Semigroup}
+import org.scalacheck.Arbitrary
+
 /**
   * @author Igor Wolkov
   */
 object CatsWorkshop extends App {
+  import cats.instances.int._
+  import cats.syntax.semigroup._
+
+  trait Stats {
+    val value: Int
+    val name: String
+  }
+
+  // Calculate some statistics data about users
+  case class UserStats(value: Int) extends Stats {
+    val name: String = "user"
+  }
+
+  // Calculate some statistics data about loading the system
+  case class LoadStats(value: Int) extends Stats {
+    val name: String = "load"
+  }
+
+  trait Updater[S <: Stats] {
+    def update(stats: S, value: Int) : S
+    def unit(value: Int): S
+  }
+
+  implicit val userStatsUpdater: Updater[UserStats] =
+    new Updater[UserStats] {
+      def update(stats: UserStats, value: Int) = stats.copy(value)
+      def unit(value: Int) = UserStats(value)
+    }
+
+  implicit val loadStatsUpdater: Updater[LoadStats] =
+    new Updater[LoadStats] {
+      def update(stats: LoadStats, value: Int) = stats.copy(value)
+      def unit(value: Int) = LoadStats(value)
+    }
+
+  implicit def statsSemigroup[S <: Stats](implicit updater: Updater[S]): Semigroup[S] =
+    (x: S, y: S) => updater.update(x, Semigroup[Int].combine(x.value, y.value))
+
+  println(
+    UserStats(10) |+| UserStats(20)
+  )
+
+  println(
+    LoadStats(10) |+| LoadStats(20)
+  )
+
+
+  // Check monoid laws:
+  // 1. Ints are closed under multiplication
+  // 2. Associative law
+  // 3. Left and right unit law
+
+  implicit def statsEq[S <: Stats]: Eq[S] =
+    (x: S, y: S) => x.name == y.name && x.value == y.value
+
+  // Generate Stats
+  implicit def statsArbitrary[S <: Stats: Eq](implicit
+                                              gen: Arbitrary[Int],
+                                              updater: Updater[S]): Arbitrary[S] =
+    Arbitrary {
+      for {
+        value <- gen.arbitrary
+      } yield
+        updater.unit(value)
+    }
+
+  GroupLaws[UserStats].semigroup(Semigroup[UserStats]).all.check()
+  GroupLaws[LoadStats].semigroup(Semigroup[LoadStats]).all.check()
 
 }
