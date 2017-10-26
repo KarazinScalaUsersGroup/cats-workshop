3288021589e56c820791a74c71c65931edc052d2
igor.wolkoff@gmail.com <igor.wolkov@kredito.de>
2017-10-26 16:49:44 +0300

    06 Incarnation semigroup

--- src/main/scala/karazinscalausersgroup/workshop/CatsWorkshop.scala
+++ src/main/scala/karazinscalausersgroup/workshop/CatsWorkshop.scala
@@ -1,8 +1,20 @@
 package karazinscalausersgroup.workshop
 
+import cats.kernel.Semigroup
+
 /**
   * @author Igor Wolkov
   */
 object CatsWorkshop extends App {
+
+  // Versioned data
+  case class Data(incarnation: Int, payload: String)
+
+  implicit val dataSemigroup: Semigroup[Data] =
+    (x: Data, y: Data) =>
+      if (x.incarnation > y.incarnation) x
+      else                               y
+
+  println(Semigroup[Data].combine(Data(1, "Hello"), Data(2, "world")))
 
 }
