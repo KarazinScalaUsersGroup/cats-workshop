df4fc9764dac38e14d9b8401f86025d407869949
igor.wolkoff@gmail.com <igor.wolkov@kredito.de>
2017-10-30 18:46:09 +0200

    12 Simple service with composite result

--- src/main/scala/karazinscalausersgroup/workshop/CatsWorkshop.scala
+++ src/main/scala/karazinscalausersgroup/workshop/CatsWorkshop.scala
@@ -1,8 +1,45 @@
 package karazinscalausersgroup.workshop
 
+import cats.{Functor, Id}
+
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.language.{higherKinds, postfixOps}
+
 /**
   * @author Igor Wolkov
   */
 object CatsWorkshop extends App {
+
+  trait Service[In, Out, C[_]] {
+    val mapper: In => Option[Out]
+
+    // Future[Option[T]]?
+    // I don't want to use pattern matching!
+    def apply(c: C[In])(implicit functor: Functor[C]): C[Option[Out]] =
+      functor.map(c)(mapper)
+  }
+
+  case class Incrementer[C[_]]() extends Service[Int, Int, C] {
+    val mapper: Int => Option[Int] = value => Option(value + 1)
+  }
+
+  case class Multiplicator[C[_]](coeff: Int) extends Service[Int, Int, C] {
+    val mapper: Int => Option[Int] = value => Option(value * coeff)
+  }
+
+
+  val productionIncrementer = Incrementer[Future]()
+  val productionMultiplicator = Multiplicator[Future](coeff = 42)
+
+  // Don't work
+  // Broke the chain
+  // val productionFlow = productionMultiplicator(productionIncrementer(Future.successful(2)))
+
+  val testIncrementer = Incrementer[Id]()
+  val testMultiplicator = Multiplicator[Id](coeff = 42)
+
+  // Don't work
+  // Broke the chain
+  // val testFlow = testMultiplicator(testIncrementer(Future.successful(2)))
 
 }
