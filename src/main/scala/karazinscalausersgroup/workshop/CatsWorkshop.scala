962703fcccc8bd684d4d894ae80325203b932785
igor.wolkoff@gmail.com <igor.wolkov@kredito.de>
2017-10-30 18:03:33 +0200

    10 Simple asynchronous service

--- src/main/scala/karazinscalausersgroup/workshop/CatsWorkshop.scala
+++ src/main/scala/karazinscalausersgroup/workshop/CatsWorkshop.scala
@@ -1,8 +1,35 @@
 package karazinscalausersgroup.workshop
 
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+import scala.language.{higherKinds, postfixOps}
+import scala.concurrent.ExecutionContext.Implicits._
+
 /**
   * @author Igor Wolkov
   */
 object CatsWorkshop extends App {
+
+  trait Service[In, Out] {
+    val mapper: In => Out
+
+    def apply(c: Future[In]): Future[Out] =
+      c.map(mapper)
+  }
+
+  case class Incrementer() extends Service[Int, Int] {
+    val mapper: Int => Int = value => value + 1
+  }
+
+  case class Multiplicator(coeff: Int) extends Service[Int, Int] {
+    val mapper: Int => Int = value => value * coeff
+  }
+
+  val productionIncrementer = Incrementer()
+  val productionMultiplicator = Multiplicator(coeff = 42)
+
+  val productionFlow = productionMultiplicator(productionIncrementer(Future.successful(2)))
+
+  println(Await.result(productionFlow, 1 second))
 
 }
