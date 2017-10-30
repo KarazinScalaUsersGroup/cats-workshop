ca5b1eb31afc8aab94e8f63c2a65193392408b6f
igor.wolkoff@gmail.com <igor.wolkov@kredito.de>
2017-10-30 19:20:16 +0200

    18 Invariant record semigroup example

--- src/main/scala/karazinscalausersgroup/workshop/CatsWorkshop.scala
+++ src/main/scala/karazinscalausersgroup/workshop/CatsWorkshop.scala
@@ -1,8 +1,20 @@
 package karazinscalausersgroup.workshop
 
+import cats._
+import cats.implicits._
+
 /**
   * @author Igor Wolkov
   */
 object CatsWorkshop extends App {
+
+  case class Record(amount: Long)
+
+  implicit val intSemigroup: Semigroup[Record] =
+    Semigroup[Long].imap(Record)(_.amount)
+
+  val res = Record(1) |+| Record(2)
+
+  println(res)
 
 }
