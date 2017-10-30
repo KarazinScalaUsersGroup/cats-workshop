0973e0ef55ac749a62651ef59966e9b31090cee1
igor.wolkoff@gmail.com <igor.wolkov@kredito.de>
2017-10-30 19:18:45 +0200

    17 Contravariant loan balance example

--- src/main/scala/karazinscalausersgroup/workshop/CatsWorkshop.scala
+++ src/main/scala/karazinscalausersgroup/workshop/CatsWorkshop.scala
@@ -1,8 +1,40 @@
 package karazinscalausersgroup.workshop
 
+import cats.functor.Contravariant
+
+import scala.language.{higherKinds, postfixOps}
+
 /**
   * @author Igor Wolkov
   */
 object CatsWorkshop extends App {
+
+  case class Loan(amount: Long)
+  case class Record(amount: Long)
+
+  val loan = Loan(42)
+  val records = Record(1) :: Record(2) :: Record(3) :: Nil
+
+  val recordsToLoan: List[Record] => Loan =
+    records => Loan(records.foldLeft(0L)((acc, rec) => acc + rec.amount))
+
+  trait Balance[T] {
+    def calculate(t: T): Long
+  }
+
+  implicit val balanceContravariant: Contravariant[Balance] = new Contravariant[Balance] {
+    def contramap[A, B](fa: Balance[A])(f: (B) => A): Balance[B] =
+      (t: B) => fa.calculate(f(t))
+  }
+
+  val loanBalance: Balance[Loan] =
+    (t: Loan) => t.amount
+
+  println(loanBalance.calculate(loan))
+
+  val recordsBalance: Balance[List[Record]] =
+    Contravariant[Balance].contramap(loanBalance)(recordsToLoan)
+
+  println(recordsBalance.calculate(records))
 
 }
