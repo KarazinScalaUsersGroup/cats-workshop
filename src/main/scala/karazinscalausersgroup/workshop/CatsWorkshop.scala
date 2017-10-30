80c27b9001f6ad62ca591a6201878b97f9d5c5f3
igor.wolkoff@gmail.com <igor.wolkov@kredito.de>
2017-10-30 18:00:00 +0200

    09 Functor hello world

--- src/main/scala/karazinscalausersgroup/workshop/CatsWorkshop.scala
+++ src/main/scala/karazinscalausersgroup/workshop/CatsWorkshop.scala
@@ -1,8 +1,17 @@
 package karazinscalausersgroup.workshop
 
+import cats.Functor
+
 /**
   * @author Igor Wolkov
   */
 object CatsWorkshop extends App {
+
+  implicit val optionFunctor: Functor[Option] = new Functor[Option] {
+    def map[A, B](value: Option[A])(func: A => B): Option[B] =
+      value.map(func)
+  }
+
+  println(Functor[Option].map(Option(42))(_.toString))
 
 }
